9325d8781dce7808253839bcc92e2eca9527e388
johnhaus <john.haus@outlook.com>
2019-04-11 19:20:51 -0300

    now loads blog content

--- src/reducers/posts_reducer.js
+++ src/reducers/posts_reducer.js
@@ -1,7 +1,11 @@
+import { FETCH_POSTS } from '../actions';
+
 export default function(state = [], action) {
   switch(action.type) {
+    case FETCH_POSTS:
+      return action.payload;
     default: {
-    return state;
+      return state;
     }
   }
 }
